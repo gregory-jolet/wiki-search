dcca6d6c9455a0eeae2494058c08dd4aa484e8d6
= <=>
2022-09-28 11:00:21 +0200

    final commit

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 const form = document.querySelector("form");
 const input = document.querySelector("input");
 const errorMsg = document.querySelector(".error");
+const loader = document.querySelector('.loader')
+const displayResult = document.querySelector(".display-result");
+
 
 form.addEventListener("submit", handleForm);
 
@@ -13,26 +16,39 @@ function handleForm(e) {
     return;
   } else {
     errorMsg.textContent = ""
+    loader.style.display = 'flex'
+    displayResult.textContent = ''
     wikiSearch(input.value)
   }
 }
 
 async function wikiSearch (searchInput) {
-    // méthode fetch pour récupérer les données en fonction de la valeur de l'input
+
+  try {
+// méthode fetch pour récupérer les données en fonction de la valeur de l'input
     const response = await fetch(
       `https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&origin=*&srlimit=20&srsearch=${searchInput}`
     );
+    // en cas d'erreur de requête
+    if (!response.ok) {
+      throw new Error (`${response.statuts}`)
+    }
     // analyse de la réponse, conversion au format json pour être utilisable 
     const data = await response.json()
     // création des cards avec les méthodes query et search  
     creatCards(data.query.search)
+  }
+  catch(error) {
+    errorMsg.textContent = `${error}`
+    loader.style.display = 'none'
+  }
+    
 }
-
-const displayResult = document.querySelector('.display-result')
 
 function creatCards(data) {
     if (!data.length) {
         errorMsg.textContent = 'Aucun résultat ...'
+        loader.style.display = 'none'
         return
     }
     // création d'une carte pour chaque élément 
@@ -49,5 +65,6 @@ function creatCards(data) {
         // Ajout de l'enfant à l'élément parent
         displayResult.appendChild(cards)
     })
+    loader.style.display = 'none'
 }
 
